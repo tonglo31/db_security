b1911ba5b0d3f4f04a7140effe3ca2b7659b1c8a
leeman <leeman160561314@gmail.com>
2022-11-20 22:25:11 +0800

    remove files

--- back/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$hostname = "db";
-$user = "proj_docker";
-$pwd = "password";
-$db = "lamp_docker";
-$conn = mysqli_connect($hostname, $user, $pwd, $db)
-			or die(mysqli_connect_error());	
-?>
